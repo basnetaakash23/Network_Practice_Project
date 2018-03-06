66ba72738d52225bd39ef96787a71a0e3ad85a3d
AakashBasnet <aakashbasnet@AakashBasnets-MacBook-Pro.local>
2018-03-05 22:25:23 -0500

    Read all the files in the binary according to their type

--- practice_project.c
+++ practice_project.c
@@ -13,29 +13,36 @@ int main(){
 	char buffer[length_of_file];
 	int num_of_bytes = fread(buffer, length_of_file, 1, ptr);
 	int j = 0;
-	for(int i =0; i < (sizeof(buffer)/sizeof(buffer[0]));){
-		//printf("%02x\n",buffer[i]);
-		/*int size = sizeof(buffer[i]);
-		printf("The size of a buffer[i+1] is %d\n", size);*/
-		//printf("The index just before entering the if conditional is %d\n",i);
-
-		/*if(buffer[i] == 0){
-			//printf("Buffer i is %02x and index is %u\n",buffer[i], i);
-			printf("The index here is %d\n", i);
-			j = first_input_type_to_second(i, buffer);
-
-		}*/
-		else if(buffer[i] == 1){
-			j = second_input_type_to_first(i, buffer);
-
-		}
-		i++;
-	}
-	return 0;
+    int i = 0;
+//    while(i < length_of_file){
+//        //printf("%02x\n",buffer[i]);
+//        /*int size = sizeof(buffer[i]);
+//        printf("The size of a buffer[i+1] is %d\n", size);*/
+//        //printf("The index just before entering the if conditional is %d\n",i);
+//        printf("The value in buffer is %u\n",buffer[i]);
+//        if(buffer[i] == 0){
+//            //printf("Buffer i is %02x and index is %u\n",buffer[i], i);
+//            printf("The index here in type 1 is %d\n", i);
+//            i = first_input_type_to_second(i, buffer);
+//            printf("The index returned here in type 1 is %d\n",i);
+//
+//        }
+//        else if(buffer[i] == 1){
+//            printf("The index here in type 2 is %d\n",i);
+//            i = second_input_type_to_first(i, buffer);
+//            printf("The index returned here in type 2 is %d\n",i);
+//        }
+//
+//    }
+    printf("The index here in type 1 is %d\n",i);
+    i = first_input_type_to_second(31, buffer);
+    printf("The index returned here in type 1 is %d\n",i);
+    return 0;
 }
 
 
 int first_input_type_to_second(int pos, char* binary_buffer){
+     printf("The index before entering type 1 is %d\n", pos);
 	int size_of_unit = 2;
 	//printf("The number in binary buffer is %d",(int)binary_buffer[pos+1]);
 	
@@ -59,42 +66,51 @@ int first_input_type_to_second(int pos, char* binary_buffer){
 		count = count + 2;
 		inner_count = inner_count + 2;
 	}
-
-	return count-1;
+     printf("The index before returning to main from type 1 is %d\n", count );
+	return count;
 }
 int second_input_type_to_first(int pos, char* binary_buffer){
+    printf("The index before entering type 2 is %d\n", pos);
 	int i = pos;
-	int amount = (binary_buffer[i+1]<<16 | binary_buffer[i+2] << 8 | binary_buffer[i+3]) ;
-	printf("The amount is %x\n",amount);
-	char second_input[3];
-	char value;
+    
+	//int amount = (binary_buffer[i+1]<<16 | binary_buffer[i+2] << 8 | binary_buffer[i+3]) ;
+    int amount = (binary_buffer[i+1]-48)*100 + (binary_buffer[i+2]-48)*10 + binary_buffer[i+3]-48;
+//    printf("The binary buffer[i+1] is %d\n",binary_buffer[i+1]-48);
+//    printf("The binary buffer[i+2] is %d\n",binary_buffer[i+2]-48);
+//    printf("The binary buffer[i+3] is %d\n",binary_buffer[i+3]-48);
+	printf("The amount is %d\n",amount);
+	char second_input[amount];
+	
 	i = i + 4;
-	int j = 1;               //for byte concatenation
-	value = binary_buffer[i];
-	while(binary_buffer[i]!=0 | binary_buffer[i]!=1){
-		if(binary_buffer[i] == '2c'){
-			value = binary_buffer[i+1];
-			i = i+1;
-			j = 1;
-			continue
-		}
-		else if(binary_buffer[i+1]){
-			value = binary_buffer[i+1]<<8*j | value;
-
-			j = j+1;
-			i = i+1;
-		}
-		
-	}
-
-
-
-
-
-	return 0;
-
-
-}
+	int count = 1;
+    //for counting the numbers of unit of the type 1 and we assume that there is at least one such unit until we find either 0 or a comma.
+    int j  = 0; //working around with concatenating the bytes of ascii values
+    int value;
+    int num_of_units = 0;
+    int comma = 44;
+    int type0 = 0;
+    int type1 = 1;
+    
+    while(num_of_units<amount){
+        if(binary_buffer[i] == 44){
+            num_of_units = num_of_units + 1;
+            i = i+1;
+        }
+        else if(binary_buffer[i] == type0 | binary_buffer[i] == type1){
+            printf("The index before returning to main from type 2 is %d\n", i);
+            return i;
+        }
+        else{
+            if(j>0){
+                value = value*10 + (binary_buffer[i]-48);
+            }
+            value = binary_buffer[i]-48;
+            j = j+1;
+            i = i+1;
+        }
+    }
+    
+ }
 
 
 
